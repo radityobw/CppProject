1b14994531156a160eedd107000e34fb1e97f8b4
ryocantsleep <ryocantsleep@gmail.com>
2025-06-26 17:21:29 +0700

    membuat kode pola segitiga siku-siku di c++

--- pola/pola-sigasiku1.cpp
+++ pola/pola-sigasiku1.cpp
@@ -9,9 +9,6 @@ int main() {
     cin >> n;
 
     for (int i = 1; i <= n; i++) {
-        for (int j = n; j > i; j--) {
-            cout << " ";
-        }
         for (int j = 1; j <= i; j++) {
             cout << "*";
         }
